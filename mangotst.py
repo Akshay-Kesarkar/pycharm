2ecb4eb59343bd4bf6917e2e5e73928466a306c7
Akshay <akshaykesarkar28@gmail.com>
2022-07-17 19:25:47 +0530

    this is my first code push in git

---
+++ mangotst.py
@@ -0,0 +1,15 @@
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb+srv://ineuron:mongodb123@cluster0.goi2j.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d= {
+    "name":"akshaykesarkar",
+    "email": "akshaykesarkar28@gmail.com",
+    "Surname":"kesarkar"
+}
+db1 = client['mongotst']
+coll = db1['test']
+coll.insert_one(d )
